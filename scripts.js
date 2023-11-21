9aef84a0fa13c2baf957d9f67d6d579f63619c61
Dewald Breed <brddew8@protonmail.com>
2023-11-21 15:06:40 +0200

    Challenge Completed

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+import { html, createOrderHtml, updateDraggingHtml, moveToColumn} from "./view.js";
+import { createOrderData } from "./data.js";
+
+
 /**
  * A handler that fires when a user drags over any element inside a column. In
  * order to determine which column the user is dragging over the entire event
@@ -9,10 +13,11 @@
  *
  * @param {Event} event 
  */
+let column = null
 const handleDragOver = (event) => {
     event.preventDefault();
     const path = event.path || event.composedPath()
-    let column = null
+    
 
     for (const element of path) {
         const { area } = element.dataset
@@ -23,19 +28,75 @@ const handleDragOver = (event) => {
     }
 
     if (!column) return
-    updateDragging({ over: column })
-    updateDraggingHtml({ over: column })
+updateDraggingHtml({ over: column })
+    
 }
 
 
-const handleDragStart = (event) => {}
-const handleDragEnd = (event) => {}
-const handleHelpToggle = (event) => {}
-const handleAddToggle = (event) => {}
-const handleAddSubmit = (event) => {}
-const handleEditToggle = (event) => {}
-const handleEditSubmit = (event) => {}
-const handleDelete = (event) => {}
+const handleHelpToggle = (event) => {
+    if (html.help.overlay.open) {
+    html.help.overlay.close()
+    } else html.help.overlay.showModal()
+}
+
+const handleAddToggle = (event) => {
+    if (html.add.overlay.open) {
+        html.add.overlay.close();
+        html.add.form.reset();
+    } else html.add.overlay.showModal()
+}
+
+const handleAddSubmit = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    const order = createOrderData(data)
+    order.column = 'ordered'
+
+    html.columns[(order.column)].appendChild(createOrderHtml(order))
+    html.add.overlay.close();
+    html.add.form.reset();
+}
+let currentTargetOrder = {}
+const handleEditToggle = (event) => {
+
+    if (event.target.classList.contains('order')) {
+        html.edit.overlay.showModal()
+    } else 
+    html.edit.overlay.close()
+    html.edit.form.reset();
+    
+    currentTargetOrder = event.target.dataset
+    return currentTargetOrder
+}
+
+const handleEditSubmit = (event) => {
+    event.preventDefault()
+    
+    const editFormData = new FormData(event.target);
+    const data = Object.fromEntries(editFormData);
+    const { id, title, table, column } = data
+
+    const orderToEdit = document.querySelector(`[data-id="${currentTargetOrder['id']}"]`)
+    orderToEdit.children[0].innerText = title
+    orderToEdit.children[1].children[0].children[1].innerText = table
+    moveToColumn(currentTargetOrder['id'], column)
+
+    html.edit.overlay.close()
+    html.edit.form.reset()
+}
+const handleDelete = (event) => {
+    document.querySelector(`[data-id="${currentTargetOrder['id']}"]`).remove()
+    html.edit.overlay.close()
+}
+
+
+
+const handleDragEnd = (event) => {
+   moveToColumn(event.target.dataset.id, column)
+   console.log(event.target.dataset)
+}
 
 html.add.cancel.addEventListener('click', handleAddToggle)
 html.other.add.addEventListener('click', handleAddToggle)
@@ -50,7 +111,6 @@ html.help.cancel.addEventListener('click', handleHelpToggle)
 html.other.help.addEventListener('click', handleHelpToggle)
 
 for (const htmlColumn of Object.values(html.columns)) {
-    htmlColumn.addEventListener('dragstart', handleDragStart)
     htmlColumn.addEventListener('dragend', handleDragEnd)
 }
 
